389e64573dd9aa9a159110f0a8eea674a3927a29
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-06-02 20:16:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -33,7 +33,7 @@ class Transfer
       #binding.pry
     receiver.balance = receiver.balance - amount
     sender.balance = sender.balance + amount
-    @status = "complete"
+    @status = "reversed"
     else
   end
 
