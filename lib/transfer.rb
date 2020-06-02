fb09f825b333355010c3c611927917df0a12c04b
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-06-02 20:25:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    binding.pry
+    #binding.pry
     if sender.balance > amount
       receiver.balance = receiver.balance + amount
       sender.balance = sender.balance - amount
@@ -24,7 +24,7 @@ class Transfer
       sender.balance < amount || sender.status == "closed"
       "Transaction rejected. Please check your account balance."
       @status = "rejected"
-    else 
+    else
     end
   end
 
