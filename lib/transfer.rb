612038adea43f320dd78af93d336f29a402a1403
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-06-02 20:12:35 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,8 +29,8 @@ class Transfer
 
   def reverse_transfer
     if @status = "complete"
-    receiver.balance = receiver.balance + amount
-    sender.balance = sender.balance - amount
+    receiver.balance = receiver.balance - amount
+    sender.balance = sender.balance + amount
     else
   end
 
