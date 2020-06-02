e0a3d31815b1c1e3340a51710ada7944749e7ff6
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-06-02 20:01:30 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,35 @@
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+   #binding.pry
+    if sender.balance > amount
+      receiver.balance = receiver.balance + amount
+      sender.balance = sender.balance - amount
+      @status = "complete"
+    elsif
+      sender.balance < amount || sender.close_account?
+      "Transaction rejected. Please check your account balance."
+      @status = "rejected"
+    else
+    end
+  end
+
+  def reverse_transfer
+
+
+  end
+
 end
